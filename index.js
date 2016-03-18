e6f826758ef21b33f4cf1e910d546fde55bd312b
EGOIST <0x142857@gmail.com>
2016-03-18 22:36:05 +0800

    clean code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const which = require('which')
 
 module.exports = function () {
   return new Promise((resolve, reject) => {
-    which('npm', function (err, npm) {
+    which('npm', (err, npm) => {
       if (err) {
         return reject(err)
       }
